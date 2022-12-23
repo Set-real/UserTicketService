b6029c63cd51a07637cbbdd3d0e78419076e5573
Name <email@example.ru>
2022-12-23 14:38:29 +0300

    Add oll files

---
+++ UserTicketServiceTests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserTicketServiceTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.True(100 == 100);
+        }
+    }
+}
